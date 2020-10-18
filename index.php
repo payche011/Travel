6002bc87318b1861e3df04f089b1e2e903947f08
Pavle Marinkovic <payche255@gmail.com>
2020-10-19 01:26:10 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include 'partials/header.php'; ?>
+
+<!--header-->
+<header class="mainHeader">
+    <h1>Vase putovanje iz snova</h1>
+    <p>Izaberite svoju avanturu</p>
+    <button>Pretrazite</button>
+</header>
+
+<!--putovanje-->
+
+    <section class="putovanje">
+        <h2>Najpopularnije destinacije</h2>
+        <p>Rezervisite do kraja meseca za 30% popusta</p>
+        <div class="container">
+            <article>
+                <img src="img/grcka.jpg" alt="">
+                <div class="text">
+                    <i class="fas fa-map-marker-alt"></i><span>Grcka</span>
+                </div>
+            </article>
+            <article>
+                <img src="img/italija.jpg" alt="">
+                <div class="text">
+                    <i class="fas fa-map-marker-alt"></i><span>Italija</span>
+                </div>
+            </article>
+            <article>
+                <img src="img/kanada.jpg" alt="">
+                <div class="text">
+                    <i class="fas fa-map-marker-alt"></i><span>Kanada</span>
+                </div>
+            </article>
+            <article>
+                <img src="img/norveska.jpg" alt="">
+                <div class="text">
+                    <i class="fas fa-map-marker-alt"></i><span>Norveska</span>
+                </div>
+            </article>
+            <article>
+                <img src="img/rusija.jpg" alt="">
+                <div class="text">
+                    <i class="fas fa-map-marker-alt"></i><span>Rusija</span>
+                </div>
+            </article>
+            <article>
+                <img src="img/spanija.jpg" alt="">
+                <div class="text">
+                    <i class="fas fa-map-marker-alt"></i><span>Spanija</span>
+                </div>
+            </article>
+        </div>
+    </section>
+
+
+
+<?php include 'partials/footer.php'; ?>
+    
---
+++ partials/footer.php
@@ -0,0 +1,34 @@
+<!--  footer  -->
+<footer>
+    <div class="container">
+        <article>
+            <h3>O nama</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam ab natus modi eos assumenda consequuntur quae libero suscipit velit, debitis nobis consectetur blanditiis aliquid eligendi. Harum culpa optio excepturi recusandae? Est temporibus ut quos quibusdam iusto sunt facere, minus repellendus.</p>
+        </article>
+        <article>
+            <h3>Top lokacija</h3>
+            <ul>
+                <li><a href="">Turska</a></li>
+                <li><a href="">Grcka</a></li>
+                <li><a href="">Hrvatska</a></li>
+                <li><a href="">Italija</a></li>
+                <li><a href="">Bugarska</a></li>
+                <li><a href="">Crna Gora</a></li>
+            </ul>
+        </article>
+        <article>
+            <h3>Newsletter</h3>
+            <p>Rane Prijave u vas inbox</p>
+            <input type="email" placeholder="Email">
+            <input type="submit" value="Posalji">
+        </article>
+    </div>
+</footer>
+
+
+
+
+
+</body>
+
+</html>
